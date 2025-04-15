1c16f9cf87d60b65c4d0758812e11f787d8ebe60
dinkyy2302 <dinkyykhurana23@gmail.com>
2025-04-15 11:46:58 +0530

    Create Employee.java

---
+++ Easy/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Name: " + name + " | Salary: " + salary;
+    }
+}
